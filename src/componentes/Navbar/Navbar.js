3bfe4db5b63eea0b71afcc952c3aba748316b8d6
leandro martinez <llggmm23@gmail.com>
2022-11-22 12:22:01 -0300

    primero cambios
    p cambios

---
+++ src/componentes/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <>
+          <div>
+            <div className="primero">hola</div>
+            <div>chau</div>
+          </div>
+        </>
+     
+     )
+};
+
+export default Navbar;
